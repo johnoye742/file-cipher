a361caee88e407cce7d63790478bfb8ab0a909d3
John Oye <olugbengajohnoye@gmail.com>
2025-03-03 11:58:25 +0100

    breaking changes

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,32 @@
-use std::{fs, io};
-
+use std::{fs, io, thread};
+use rsa::RsaPrivateKey;
+use rand;
 
 
 fn main() {
     println!("File Encryptor Prototype v1.0");
     println!("Input file location: ");
 
+    let mut content = Vec::new();
     let mut file_path = String::new();
     let _ = io::stdin().read_line(&mut file_path);
+
+    let mut rng = rand::thread_rng();
+    let privateKey = RsaPrivateKey::new(&mut rng, 2048);
     
     match fs::read(file_path.trim()) {
         Ok(lines) => {
             println!("{:?}", lines);
+            for line in lines {
+                if line < 1 {
+                    &content.push(line);
+                } else {
+                    &content.push(line - 1);
+                }
+            }
         },
         Err(err) => println!("{:?}", err)
     }
+
+    fs::write("../moded-file", content);
 }
