d0e62c4c703120e5bd921c269e98b8c0f9205e93
johnoye742 <olugbengajohnoye@gmail.com>
2025-03-05 11:22:29 +0100

    Now properly does encryption and decryption using passphrase

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,12 @@
 use std::{fs, io::{self, Read}};
 
-use aes_gcm::{aead::{Aead, OsRng}, AeadCore, Aes256Gcm, Key, KeyInit};
+use aes_gcm::{aead::{Aead, OsRng}, AeadCore, Aes256Gcm, Key, KeyInit, Nonce};
 
 
 fn main() {
     println!("File Encryptor Prototype v1.0");
     println!("Input file location: ");
 
-    let mut content = Vec::new();
     let mut file_path = String::new();
     let _ = io::stdin().read_line(&mut file_path);
 
@@ -17,11 +16,25 @@ fn main() {
 
     let _ = io::stdin().read_line(&mut passphrase);
 
+    println!("Encrypt File (y/n) [if n decrypt] :");
+    let mut encrypt = String::new();
     
+    io::stdin().read_line(&mut encrypt).expect("Didn't get that");
+
+    if encrypt.to_lowercase().starts_with("y") {
+        encrypt_file(&passphrase, &file_path);
+    } else {
+        decrypt_file(&passphrase, &file_path);
+    }
+    
+}
+
+fn encrypt_file(passphrase: &String, file_path: &String) {
+    let mut content = Vec::new();
     let key = Key::<Aes256Gcm>::from_slice(passphrase.trim().as_bytes());
 
     let cipher = Aes256Gcm::new(key);
-    let nonce = Aes256Gcm::generate_nonce(&mut OsRng);
+    let nonce = Nonce::from_slice(&passphrase[0..12].as_bytes());
     match fs::read(file_path.trim()) {
         Ok(lines) => {
             for line in lines {
@@ -36,18 +49,16 @@ fn main() {
         Ok(text) => text,
         Err(err) => panic!("{:?}", err)
     };
-    fs::write("../moded-file", encrypted).expect("Cpuldn't write to file");
-    println!("{:?}", key);
+    fs::write(file_path.trim(), encrypted).expect("Couldn't write to file");
+    println!("Nonce size: {}", nonce.len());
 }
 
-fn encrypt_file(passphrase: String, file_path: String) {
-
-}
-
-fn decrypt_file (passphrase: String, file_path: String) {
+fn decrypt_file (passphrase: &String, file_path: &String) {
     let key = Key::<Aes256Gcm>::from_slice(passphrase.trim().as_bytes());
     let cipher = Aes256Gcm::new(key);
     let mut content = Vec::new();
+    let nonce = Nonce::from_slice(&passphrase[0..12].as_bytes());
+
     match fs::read(file_path.trim()) {
         Ok(bytes) => {
             for byte in bytes {
@@ -56,7 +67,11 @@ fn decrypt_file (passphrase: String, file_path: String) {
         },
         Err(err) => println!("{:?}", err)
     }
-    let decrypted = cipher.decrypt(&Aes256Gcm::generate_nonce(&mut OsRng), content.as_ref());
 
-    fs::write(file_path, &decrypted.expect("couldn't decrypt file")).expect("couldn't write the file");
+    let decrypted = cipher.decrypt(&nonce, content.as_ref());
+
+    fs::write(file_path.trim(), match &decrypted {
+        Ok(d) => d,
+        Err(err) => panic!("{:?}", err)
+    }).expect("couldn't write the file");
 }
